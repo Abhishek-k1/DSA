7e878f96966b459a2ce227553f907e9790174499
Abhishek-k1 <abhishek97766@gmail.com>
2026-09-09 20:03:51 +0530

    Update two-sum.cpp

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,3 +1,18 @@
+// Leetcode #1 - Two Sum
+//
+// Approach:
+// Check every possible pair of numbers and return their indices
+// When their sum is equal to the target
+//
+// Steps:
+// 1. Start with the first element
+// 2. Compare it with every element after it
+// 3. Check whether numa[i] + nums[j] == target
+// 4. If the sum equals target, return the two indices
+//
+// Time Complexity: O(n²) 
+// Space Complexity: O(1)
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
