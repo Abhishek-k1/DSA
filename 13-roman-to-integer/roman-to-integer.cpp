1708204a178490244b8455830733b4ed4dfa4b1e
Abhishek-k1 <abhishek97766@gmail.com>
2026-09-13 23:06:32 +0530

    Update roman-to-integer.cpp

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,3 +1,34 @@
+// LeetCode #13 - Roman to Integer
+//
+// Problem:
+// Convert a Roman numeral string into an integer
+//
+// Approach:
+// Store the value of each Roman Numeral in a map
+// Traverse the string from left to right
+// If the current Roman numeral is smaller than the next one
+// subtract its value
+// Otherwise, add its value
+// Finally, add the value of the last Roman numeral
+//
+// Steps:
+// 1. Create a map to store Roman numeral values
+// 2.Store values of I, V, X, L, C, D and M
+// 3.Create a variable result = 0
+// 4.Traverse the string except the last character
+// 5.Compare the current character with the next character
+// 6. If Current value is smaller than next value, subtract it
+// 7. Otherwise, add it
+// 8. Add the value of the last character
+// 9. Return the result
+//
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+//
+// n = length of the Roman numeral string
+// The map contains only 7 fixed Roman numeral characters
+
+
 class Solution {
 public:
     int romanToInt(string s) {
